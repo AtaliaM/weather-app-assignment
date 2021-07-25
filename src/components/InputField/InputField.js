795b8523c09a98e1eb74483aae1e8a6714493c65
Atalia Mucharsky <atalia.mucharsky@hotmail.com>
2021-07-26 00:32:31 +0300

    edited InputField with 'no results' msg, edited styles, working on userInputCheck

--- src/components/InputField/InputField.js
+++ src/components/InputField/InputField.js
@@ -11,13 +11,13 @@ function InputField(props) {
     const [term, setTerm] = useState("");
     const [noResultsFound, setNoResultsFound] = useState(false);
 
-    const onTermSubmit = async() => {
+    const onTermSubmit = async () => {
         try {
             const inputCheckResult = userInputCheck(term);
             if (inputCheckResult) {
                 const res = await weather.get(`search/?query=${inputCheckResult}`);
                 console.log(res.data)
-                if(res.data.length!==0) {
+                if (res.data.length !== 0) {
                     props.chooseCity(res.data[0])
                     setNoResultsFound(false);
                 }
@@ -26,38 +26,39 @@ function InputField(props) {
                     props.fetchWeather(null);
                     props.chooseCity(null);
                 }
-            
+
             }
             //if term returns one city- pass the city to our action creator 'chooseCity',
             //if there are user typos, check for match within cities dataset,
             //if multiple cities returned, 
-        } catch(e) {
+        } catch (e) {
             console.log(e);
         }
     }
 
     const onKeySubmit = (e) => {
-        if(e.key === 'Enter'){
-           onTermSubmit();
-          }
+        if (e.key === 'Enter') {
+            onTermSubmit();
+        }
     }
 
     return (
         <div className="input-container">
-            <div className="ui fluid action input">
-                <input type="text" placeholder="Search City" onKeyPress={(e)=>onKeySubmit(e)} onChange={(e)=>setTerm(e.target.value)}/>
-                <button className="ui button searchbtn" onClick={()=>onTermSubmit()}>Search</button>
+            <div className="ui fluid action input left icon">
+                <i className="search icon"></i>
+                <input type="text" placeholder="Search City" onKeyPress={(e) => onKeySubmit(e)} onChange={(e) => setTerm(e.target.value)} />
+                <button className="ui button searchbtn" onClick={() => onTermSubmit()}>Search</button>
             </div>
-            {noResultsFound ? 
-            <h4 className="no-results-msg">Sorry, no results found for this location</h4>
-            : null}
+            {noResultsFound ?
+                <h4 className="no-results-msg">Sorry, no results found for this location</h4>
+                : null}
         </div>
     )
 }
 
 const mapStateToProps = (state) => {
     console.log(state)
-    return( {
+    return ({
         chosenCity: state.chosenCity,
         cities: state.cities
     })
