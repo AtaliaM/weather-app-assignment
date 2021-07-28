4b161f08385ee90b2b09d9dcc4246f3c3ee6d477
Atalia Mucharsky <atalia.mucharsky@hotmail.com>
2021-07-28 21:25:05 +0300

    refactor

--- src/components/InputField/InputField.test.js
+++ src/components/InputField/InputField.test.js
@@ -1,14 +1,10 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import InputField from './InputField';
 import configureStore from 'redux-mock-store';
 import userEvent from '@testing-library/user-event';
 import { fireEvent, render, screen } from '@testing-library/react';
 import { chosenCityReducer, chosenCityWeatherReducer } from '../../reducers';
-
-// const chooseCity = (city) => ({ type: 'CITY_CHOSEN', payload: city });
-// const fetchWeather = (weather) => ({ type: 'WEATHER_FETCHED', payload: weather })
 
 describe("<InputField />", () => {
     
@@ -47,21 +43,6 @@ describe("<InputField />", () => {
       const inputEl = screen.getByTestId("search-field");
       userEvent.type(inputEl, "paris");
       expect(screen.getByTestId("search-field")).toHaveValue("paris");
-    });
-
-
-    test('pass a random input to test that "no result" message appears', () => {
-      renderComponent()
-
-      const inputEl = screen.getByTestId("search-field");
-      userEvent.type(inputEl, "notacity");
-      expect(screen.getByTestId("search-field")).toHaveValue("notacity");
-
-      const buttonEl = screen.getByTestId("search-btn")
-      fireEvent.click(buttonEl)
-
-    //   expect(screen.queryByTestId("not-found-msg")).toBeInTheDocument();
-    //   expect(screen.queryByTestId("not-found-msg").textContent).toEqual("Sorry, no results found for this location");
     });
 
 });
